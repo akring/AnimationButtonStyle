a47bff8ebd47939c06f15b3ec39c7087aa2fd746
Akring <ddflj3210@gmail.com>
2023-07-08 20:41:27 +0800

    Initial Commit

---
+++ AnimationTest/AnimationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTestApp.swift
+//  AnimationTest
+//
+//  Created by 吕俊 on 2023/7/8.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationTest
+//
+//  Created by 吕俊 on 2023/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
