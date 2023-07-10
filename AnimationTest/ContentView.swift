3a03fd695a2d6c0a871eeb1c2d032755078923df
Akring <ddflj3210@gmail.com>
2023-07-10 11:15:28 +0800

    Initial Commit

--- AnimationTest/ContentView.swift
+++ AnimationTest/ContentView.swift
@@ -10,12 +10,55 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                print("Clicked")
+            }, label: {
+                Text("Click me!")
+            })
+            .buttonStyle(AnimationButtonStyle())
+            .padding(.bottom, 200)
+            
+            
+            Button(action: {
+                print("Clicked")
+            }, label: {
+                Text("Click me!")
+            })
+            .buttonStyle(AnimationButtonStyle(full: true))
         }
-        .padding()
+    }
+}
+
+struct AnimationButtonStyle: ButtonStyle {
+    @State var rotation: CGFloat = 0.0
+    
+    var full: Bool = false
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background {
+                RoundedRectangle(cornerRadius: 20.0, style: .continuous)
+                    .frame(width: 200, height: 100)
+                    .foregroundColor(Color(white: 0.15))
+                    .shadow(color: .black.opacity(0.5), radius: 10)
+                RoundedRectangle(cornerRadius: 20.0, style: .continuous)
+                    .frame(width: full ? 260 : 60, height: 220)
+                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.red, .purple, .blue]),
+                                                    startPoint: .top, endPoint: .bottom))
+                    .rotationEffect(.degrees(rotation))
+                    .mask {
+                        RoundedRectangle(cornerRadius: 20.0, style: .continuous)
+                            .stroke(lineWidth: 5)
+                            .frame(width: 196, height: 96)
+                    }
+            }
+            .foregroundColor(.white)
+            .font(.title)
+            .onAppear {
+                withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
+                    rotation = 360
+                }
+            }
     }
 }
 
